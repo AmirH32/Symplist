910df472f06b47a9d30aaeb443e193c6679bd02d
Amir <109604111+AmirH32@users.noreply.github.com>
2023-02-04 20:04:22 +0000

    Delete website directory
    - Removed the 'website' folder as I decided on designing and creating my project in a different formet

--- website/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from flask_login import UserMixin
-from sqlalchemy import func
-
-# Creates a class for each table entity in the database (object relation mapping)
-class Patient(db.Model):
-    PatientID = db.Column(db.Integer, primary_key=True)
-    Email = db.Column(db.String(256), nullable=False, unique=True)
-    Password = db.Column(db.String(128), nullable=False)
