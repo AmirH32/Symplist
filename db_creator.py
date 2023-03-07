768da36af4d408474461d4417e1456e04ec9eb5a
Amir <109604111+AmirH32@users.noreply.github.com>
2023-03-07 13:49:34 +0000

    Update db_creator.py
    - db creator now adds the two default roles to the database automatically.

--- db_creator.py
+++ db_creator.py
@@ -1,3 +1,10 @@
-from Symplist import app, db
+from Symplist import app, db, Roles
+
 app.app_context().push()
 db.create_all() 
+
+doctor = Roles(Role_Name="Doctor")
+patient = Roles(Role_Name="Patient")
+db.session.add(doctor)
+db.session.add(patient)
+db.session.commit()
